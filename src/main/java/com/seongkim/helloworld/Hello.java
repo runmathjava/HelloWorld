06c8c0ab71bbc775cd531d4653fac09b32045170
runmathjava <85817730+runmathjava@users.noreply.github.com>
2021-06-26 22:50:28 -0600

    First Commit

---
+++ src/main/java/com/seongkim/helloworld/Hello.java
@@ -0,0 +1,14 @@
+/*
+ *@author Seong Kim
+ *email: aboxinabox@hotmail.com
+ *date: June 26 2021
+ *purpose: My first Hello World program in an IDE
+ */
+package com.seongkim.helloworld;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Everyone!");
+    }
+    
+}
